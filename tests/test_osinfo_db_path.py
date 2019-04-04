34ff01e64ac9fae5b8fcd8c8f6f94016880063c3
Fabiano Fidêncio <fidencio@redhat.com>
2019-04-04 12:34:48 +0200

    tests: Add osinfo-db-path tests
    Add a set of functional tests to ensure osinfo-db-path works as
    expected. The covered scenarios are:
    - osinfo-db-path --system
    - osinfo-db-path --local
    - osinfo-db-oath --user
    - osinfo-db-path --dir
    - osinfo-db-path --root ROOT --system
    
    https://gitlab.com/libosinfo/osinfo-db-tools/issues/2
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Cole Robinson <crobinso@redhat.com>

---
+++ tests/test_osinfo_db_path.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+#
+# This work is licensed under the GNU GPLv2 or later.
+# See the COPYING file in the top-level directory.
+
+import os
+import sys
+import pytest
+import util
+
+
+DATADIR = os.environ.get("datadir")
+SYSCONFDIR = os.environ.get("sysconfdir")
+FOOBAR_DIR = "/foo/bar"
+
+
+def test_osinfo_db_path_system():
+    """
+    Test osinfo-db-path --system
+    """
+    cmd = [util.Tools.db_path, util.ToolsArgs.SYSTEM]
+    output = util.get_output(cmd)
+    expected_output = os.path.join(DATADIR, "osinfo\n")
+    assert output == expected_output
+
+
+def test_osinfo_db_path_local():
+    """
+    Test osinfo-db-path --local
+    """
+    cmd = [util.Tools.db_path, util.ToolsArgs.LOCAL]
+    output = util.get_output(cmd)
+    expected_output = os.path.join(SYSCONFDIR, "osinfo\n")
+    assert output == expected_output
+
+
+def test_osinfo_db_path_user():
+    """
+    Test osinfo-db-path --user
+    """
+    cmd = [util.Tools.db_path, util.ToolsArgs.USER]
+    output = util.get_output(cmd)
+    expected_output = os.path.join(os.environ["HOME"], ".config",
+                                   "osinfo\n")
+    assert output == expected_output
+
+
+def test_osinfo_db_path_dir():
+    """
+    Test osinfo-db-path --dir
+    """
+    cmd = [util.Tools.db_path, util.ToolsArgs.DIR, FOOBAR_DIR]
+    output = util.get_output(cmd)
+    expected_output = FOOBAR_DIR + "\n"
+    assert output == expected_output
+
+
+def test_osinfo_db_path_root():
+    """
+    Test osinfo-db-path --root FOOBAR_DIR --system
+    """
+    cmd = [util.Tools.db_path, util.ToolsArgs.ROOT, FOOBAR_DIR,
+           util.ToolsArgs.SYSTEM]
+    output = util.get_output(cmd)
+    expected_output = os.path.join(FOOBAR_DIR, *DATADIR.split("/"), "osinfo\n")
+    assert output == expected_output
+
+
+if __name__ == "__main__":
+    exit(pytest.main(sys.argv))
