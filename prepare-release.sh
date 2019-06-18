85e65c01429915e534d0b3c26c4f4a3f628428d6
Fabiano Fidêncio <fidencio@redhat.com>
2019-06-18 17:17:54 +0200

    prepare-release: Adapt to use meson
    Let's adapt the prepare-release script to use meson build system instead
    of autotools.
    
    This change has been tested and is working fine on Fedora 30+.
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Cole Robinson <crobinso@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -7,86 +7,63 @@ test -n "$1" && RESULTS=$1 || RESULTS=results.log
 INSTALL_ROOT=$HOME/builder
 
 # Make things clean.
-test -f Makefile && make -k distclean || :
-
 rm -rf build
-mkdir build
-cd build
+meson build/native --prefix=$INSTALL_ROOT --werror
 
-../autogen.sh --prefix=$INSTALL_ROOT \
-    --enable-werror
-
-# If the MAKEFLAGS envvar does not yet include a -j option,
-# add -jN where N depends on the number of processors.
-case $MAKEFLAGS in
-  *-j*) ;;
-  *) n=$(getconf _NPROCESSORS_ONLN 2> /dev/null)
-    test "$n" -gt 0 || n=1
-    n=$(expr $n + 1)
-    MAKEFLAGS="$MAKEFLAGS -j$n"
-    export MAKEFLAGS
-    ;;
-esac
-
-make
-make install
+ninja -C build/native
+ninja -C build/native install
 
 # set -o pipefail is a bashism; this use of exec is the POSIX alternative
 exec 3>&1
 st=$(
   exec 4>&1 >&3
-  { make check syntax-check 2>&1 3>&- 4>&-; echo $? >&4; } | tee "$RESULTS"
+  { ninja -C build/native test 2>&1 3>&- 4>&-; echo $? >&4; } | tee "$RESULTS"
 )
 exec 3>&-
 test "$st" = 0
 
-rm -f *.tar.gz
-make dist
+ninja -C build/native dist
 
-if [ -f /usr/bin/rpmbuild ]; then
+if test -f /usr/bin/rpmbuild; then
   rpmbuild --nodeps \
-     --define "_sourcedir `pwd`" \
-     -ba --clean osinfo-db-tools.spec
+     --define "_sourcedir `pwd`/build/native/meson-dist/" \
+     -ba --clean build/native/osinfo-db-tools.spec
 fi
 
 # Test mingw32 cross-compile
-if test -x /usr/bin/i686-w64-mingw32-gcc ; then
-  make distclean
+if test -x /usr/bin/i686-w64-mingw32-gcc && \
+   test -r /usr/share/mingw/toolchain-mingw32.meson ; then
+  rm -rf build/win32 || :
 
-  PKG_CONFIG_PATH="$INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
-  CC="i686-w64-mingw32-gcc" \
-  ../configure \
-    --build=$(uname -m)-pc-linux \
-    --host=i686-w64-mingw32 \
-    --prefix="$INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw" \
-    --enable-werror
+  meson build/win32 \
+        --werror \
+        --prefix="$INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw" \
+        --cross-file="/usr/share/mingw/toolchain-mingw32.meson"
 
-  make
-  make install
-
+  ninja -C build/win32
+  ninja -C build/win32 install
 fi
 
 # Test mingw64 cross-compile
-if test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
-  make distclean
+if test -x /usr/bin/x86_64-w64-mingw32-gcc && \
+   test -r /usr/share/mingw/toolchain-mingw64.meson ; then
+  rm -rf build/win64 || :
 
-  PKG_CONFIG_PATH="$INSTALL_ROOT/x86_64-w64-mingw32/sys-root/mingw/lib/pkgconfig" \
-  CC="x86_64-w64-mingw32-gcc" \
-  ../configure \
-    --build=$(uname -m)-pc-linux \
-    --host=x86_64-w64-mingw32 \
-    --prefix="$INSTALL_ROOT/i686-w64-mingw32/sys-root/mingw" \
-    --enable-werror
+  meson build/win64 \
+        --werror \
+        --prefix="$INSTALL_ROOT/x86_64-w64-mingw32/sys-root/mingw" \
+        --cross-file="/usr/share/mingw/toolchain-mingw64.meson"
 
-  make
-  make install
-
+  ninja -C build/win64
+  ninja -C build/win64 install
 fi
 
-if test -x /usr/bin/i686-w64-mingw32-gcc && test -x /usr/bin/x86_64-w64-mingw32-gcc ; then
-  if test -f /usr/bin/rpmbuild ; then
-    rpmbuild --nodeps \
-       --define "_sourcedir `pwd`" \
-       -ba --clean mingw-osinfo-db-tools.spec
-  fi
+if test -x /usr/bin/i686-w64-mingw32-gcc && \
+   test -r /usr/share/mingw/toolchain-mingw32.meson && \
+   test -x /usr/bin/x86_64-w64-mingw32-gcc && \
+   test -r /usr/share/mingw/toolchain-mingw64.meson && \
+   test -f /usr/bin/rpmbuild; then
+  rpmbuild --nodeps \
+     --define "_sourcedir `pwd`/build/native/meson-dist/" \
+     -ba --clean build/native/mingw-osinfo-db-tools.spec
 fi
