5410c9533ac788b3f6a15a31afe5641b4e62f555
Andrea Bolognani <abologna@redhat.com>
2020-04-17 19:57:04 +0200

    prepare-release: Define BUILD_ROOT
    This fits nicely with INSTALL_ROOT and the recently-introduced
    RPMBUILD_ROOT; additionally, since $BUILD_ROOT is an absolute path,
    we can use it directly as _sourcedir when calling rpmbuild.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>

--- prepare-release.sh
+++ prepare-release.sh
@@ -3,46 +3,47 @@
 set -e
 set -v
 
+BUILD_ROOT="$PWD/build"
 INSTALL_ROOT="$PWD/install"
 RPMBUILD_ROOT="$PWD/rpmbuild"
 
 # Make things clean.
-rm -rf build "$INSTALL_ROOT" "$RPMBUILD_ROOT"
+rm -rf "$BUILD_ROOT" "$INSTALL_ROOT" "$RPMBUILD_ROOT"
 
-meson build/native --werror
+meson "$BUILD_ROOT/native" --werror
 
-ninja -C build/native
-DESTDIR="$INSTALL_ROOT/native" ninja -C build/native install
-ninja -C build/native test
-ninja -C build/native dist
+ninja -C "$BUILD_ROOT/native"
+DESTDIR="$INSTALL_ROOT/native" ninja -C "$BUILD_ROOT/native" install
+ninja -C "$BUILD_ROOT/native" test
+ninja -C "$BUILD_ROOT/native" dist
 
 if test -x /usr/bin/rpmbuild; then
   rpmbuild --nodeps \
      --define "_topdir $RPMBUILD_ROOT" \
-     --define "_sourcedir `pwd`/build/native/meson-dist/" \
-     -ba --clean build/native/osinfo-db-tools.spec
+     --define "_sourcedir $BUILD_ROOT/native/meson-dist/" \
+     -ba --clean "$BUILD_ROOT/native/osinfo-db-tools.spec"
 fi
 
 # Test mingw32 cross-compile
 if test -x /usr/bin/i686-w64-mingw32-gcc && \
    test -r /usr/share/mingw/toolchain-mingw32.meson ; then
-  meson build/win32 \
+  meson "$BUILD_ROOT/win32" \
         --werror \
         --cross-file="/usr/share/mingw/toolchain-mingw32.meson"
 
-  ninja -C build/win32
-  DESTDIR="$INSTALL_ROOT/win32" ninja -C build/win32 install
+  ninja -C "$BUILD_ROOT/win32"
+  DESTDIR="$INSTALL_ROOT/win32" ninja -C "$BUILD_ROOT/win32" install
 fi
 
 # Test mingw64 cross-compile
 if test -x /usr/bin/x86_64-w64-mingw32-gcc && \
    test -r /usr/share/mingw/toolchain-mingw64.meson ; then
-  meson build/win64 \
+  meson "$BUILD_ROOT/win64" \
         --werror \
         --cross-file="/usr/share/mingw/toolchain-mingw64.meson"
 
-  ninja -C build/win64
-  DESTDIR="$INSTALL_ROOT/win64" ninja -C build/win64 install
+  ninja -C "$BUILD_ROOT/win64"
+  DESTDIR="$INSTALL_ROOT/win64" ninja -C "$BUILD_ROOT/win64" install
 fi
 
 if test -x /usr/bin/i686-w64-mingw32-gcc && \
@@ -52,6 +53,6 @@ if test -x /usr/bin/i686-w64-mingw32-gcc && \
    test -x /usr/bin/rpmbuild; then
   rpmbuild --nodeps \
      --define "_topdir $RPMBUILD_ROOT" \
-     --define "_sourcedir `pwd`/build/native/meson-dist/" \
-     -ba --clean build/native/mingw-osinfo-db-tools.spec
+     --define "_sourcedir $BUILD_ROOT/native/meson-dist/" \
+     -ba --clean "$BUILD_ROOT/native/mingw-osinfo-db-tools.spec"
 fi
